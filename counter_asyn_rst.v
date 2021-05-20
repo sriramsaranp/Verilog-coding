aee03087e4ee76e68f8e796d35fc48cf18bae63b
sriramsaranp <psriramsaran@gmail.com>
2021-05-20 17:24:00 +0530

    Updated the code

--- counter_asyn_rst.v
+++ counter_asyn_rst.v
@@ -1,12 +1,12 @@
 //4 bit counter using Asynchronous Reset
 
-module counter_asyn_rst (clk, count, rst);
+module counter_asyn_rst(clk, count, rst);
 input clk, rst;
 output reg [3:0] count;
 
 always @ (posedge clk, posedge rst)
-initial begin
-    if(rst) count <= 4'b0;
+
+    if(rst) count <= 0;
     else count = count + 1;
-end
+    
 endmodule
