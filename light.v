c76fc61fcc6468b1fc260afc9205a4345e1cbb73
sriramsaranp <psriramsaran@gmail.com>
2021-05-23 08:48:00 +0530

    Reduced the no. of flipflops

--- light.v
+++ light.v
@@ -5,13 +5,20 @@ reg [2:0] state;
 parameter s0 = 0, s1 = 1, s2 = 2;
 parameter RED = 3'b100, GREEN = 3'b010, YELLOW = 3'b001;
 
-always @(posedge clk) begin
+always @(posedge clk) 
     case (state)
-    s0 : begin light <= GREEN; state <= s1; end
-    s1 : begin light <= YELLOW; state <= s2; end
-    s2 : begin light <= RED; state <= s0; end
-    default : begin light <= RED; state <= s0; end
+    s0 :  state <= s1; 
+    s1 :  state <= s2; 
+    s2 :  state <= s0; 
+    default :  state <= s0; 
     endcase
-end
+
+always @(state)
+case (state)
+s0 : light = RED;
+s1 : light = GREEN;
+s2 : light = YELLOW;
+default : light = RED;
+endcase
     
 endmodule
